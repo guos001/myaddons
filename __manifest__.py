f48961526512f146bb88a6696c7e1fef4ef64c12
guos001 <33093075+guos001@users.noreply.github.com>
2019-10-16 10:02:01 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'osbzr_traning',
+    'version': '1.0',
+    'author': 'aa',
+    'category': 'osbzr',
+    'description':
+    """
+    用于开阖openerp培训实例
+    """,
+    'depends': ['mail'],
+    'auto_install': False,
+    # 'data': ["security/tr_security.xml", "report.xml", "osbzr_training_view.xml",  "security/ir.model.access.csv"],
+    # 'demo': ['data/training_demo.xml']
+}
